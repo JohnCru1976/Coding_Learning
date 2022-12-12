4c8a6fa12f2ca2b75095b62451c31c4b00707329
JohnCru1976 <juanbcru@gmail.com>
2022-12-12 10:51:58 +0100

    Bash Scritp determinate the file type

---
+++ Scripting_Linux/script_determineFile.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Test with files  -e (exist) -d (directory) -f (file)
+# -r (is readable?) -w (writeable?) -x (exeecutable?)
+if [ -e $1 ]; then
+  if [ -d $1 ]; then
+    echo $1 is a directory
+  fi
+  if [ -f $1 ]; then
+    echo $1 is a file
+  fi
+else
+  echo $1 does not exist
+fi
