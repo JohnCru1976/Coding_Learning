ede2f0dab0f8145a5596d60830130c677efd00bb
JohnCru1976 <juanbcru@gmail.com>
2022-12-14 11:14:44 +0100

    Singles changes inside a script

--- Scripting_Linux/project_backup.sh
+++ Scripting_Linux/project_backup.sh
@@ -19,4 +19,35 @@ else
   echo $1 doesn\'t exists
 fi
 
+# More complex example from https://ryanstutorials.net/linuxtutorial/scripting.php
+#!/bin/bash
+# Backs up a single project directory
+# Ryan 14/12/2022
+ 
+if [ $# != 1 ]
+then
+    echo Usage: A single argument which is the directory to backup
+    exit
+fi
+if [ ! -d ~/projects/$1 ]
+then
+    echo 'The given directory does not seem to exist (possible typo?)'
+    exit
+fi
+date=`date +%F`
+ 
+# Do we already have a backup folder for todays date?
+if [ -d ~/projectbackups/$1_$date ]
+then
+    echo 'This project has already been backed up today, overwrite?'
+    read answer
+    if [ $answer != 'y' ]
+    then
+        exit
+    fi
+else
+    mkdir ~/projectbackups/$1_$date
+fi
+cp -R ~/projects/$1 ~/projectbackups/$1_$date
+
 
