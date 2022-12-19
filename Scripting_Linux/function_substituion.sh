5b99c0416e424d20f7a461107939b3828b75a1ab
johncru1976 <juanbcru@gmail.com>
2022-12-19 13:12:45 +0100

    Another example with function return

--- Scripting_Linux/function_substituion.sh
+++ Scripting_Linux/function_substituion.sh
@@ -6,3 +6,16 @@ lines_in_file () {
 }
 num_lines=$( lines_in_file $1 )
 echo The file $1 has $num_lines lines in it.
+
+
+# Another example made from myself
+#! /bin/bash
+
+function test {
+ echo "Parameter inside the function: $1" | grep 'Parameter'
+}
+
+var=$( test $2 )
+
+echo "Parameter outside the function: $1"
+echo $var
