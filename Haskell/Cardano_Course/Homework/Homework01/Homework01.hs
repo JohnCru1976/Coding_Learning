a8d59975822e3bd52f0c285e1bba76ba9c5c220a
JohnCru1976 <juanbcru@gmail.com>
2022-12-27 17:27:40 +0100

    Homework01 done

--- Haskell/Cardano_Course/Homework/Homework01/Homework01.hs
+++ Haskell/Cardano_Course/Homework/Homework01/Homework01.hs
@@ -1,16 +1,34 @@
-
+main :: IO()
+main = do
+    print (prodBy3 6)
+    print (circleArea 5)
+    print (cylinderVolume 2.8 2.4)
+    print (cylinderVolGt42 2.4 2.6)
 -- Question 1
 -- Write a multiline comment below.
+{-
+This is a multiline
+comment 
+-}
 
 -- Question 2
 -- Define a function that takes a value and multiplies it by 3.
+prodBy3 :: Num a => a -> a
+prodBy3 x = x * 3
 
 -- Question 3
 -- Define a function that calculates the area of a circle.
+circleArea :: Floating a => a -> a
+circleArea r = pi * r^2
 
 -- Question 4
 -- Define a function that calculates the volume of a cylinder by composing the previous function together with the height of the cylinder. 
+cylinderVolume :: Floating a => a -> a -> a
+cylinderVolume h r = h * circleArea r
 
 -- Question 5
 -- Define a function that takes the height and radius of a cylinder and checks if the volume is greater than or equal to 42.
-
+cylinderVolGt42 :: (Ord a, Floating a) => a -> a -> String
+cylinderVolGt42 h r = if (h * circleArea r) >= 42.0 
+    then "The volume of the cylinder is equal to or greater than 42" 
+    else "The volume of the cylinder is lesser than 42"
