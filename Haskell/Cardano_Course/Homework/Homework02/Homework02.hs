a1afb1d2a1aa5c927cedbeeaed30c88e7a13c1f3
JohnCru1976 <juanbcru@gmail.com>
2022-12-28 10:45:43 +0100

    Homework 2 done

--- Haskell/Cardano_Course/Homework/Homework02/Homework02.hs
+++ Haskell/Cardano_Course/Homework/Homework02/Homework02.hs
@@ -1,7 +1,4 @@
 
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use :" #-}
-{-# HLINT ignore "Redundant ==" #-}
 -- Question 1
 -- Add the type signatures for the functions below and then remove the comments and try to compile.
 -- (Use the types presented in the lecture.)
@@ -21,16 +18,38 @@ f4 x y z = x == (y ++ z)
 
 -- Question 2
 -- Why should we define type signatures of functions? How can they help you? How can they help others?
-
+{-
+    Signatures prevent us to make mistakes using values of a different type,
+    thus the compiler launches an exception when we tray to do so.
+    Signature let other and ourselves to know how many paramenters has tha function,
+    their type, and the typy of the value the functin returns.
+-}
 
 -- Question 3
 -- Why should you define type signatures for variables? How can they help you?
-
+{-
+    I think this is because we can use complex expressions in the definition not being
+    clear what kind of type it returns. The signature permits a quick glance of the definition type. 
+-}
 
 -- Question 4
 -- Are there any functions in Haskell that let you transform one type to the other? Try googling for the answer.
-
+{-
+    Yes, we can convert a type to an another with functions. For example:
+    (Int to Double) fromIntegral x :: Double
+    (Double to Int) round x
+    (String to Int) read s :: Int
+    (String to Double) read s :: Double
+    (Int/Double.. to String) show x
+-}
 
 -- Question 5
 -- Can you also define in Haskell list of lists? Did we showed any example of that? How would you access the inner
 -- most elements?
+{- 
+    Yes, we can define the variable l = [[1,2,3],[4,5,6],[7,8,9]]
+    I don't think so we have used any example of list of lists, at least than we consider
+    unitary elements as lists of one elements.
+    Example to acces to inner elements: [[1,2,3],[4,5,6],[7,8,9]] !! 1 !! 0 ---> 4
+-}
+
