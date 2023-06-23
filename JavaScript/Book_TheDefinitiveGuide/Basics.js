c91b674f44eb00db5d7d554f131cf5b6e1b29bf8
Juan Bautista Cru Murillo <juanbcru@hotmail.com>
2023-06-23 17:19:27 +0200

    Update basics

--- JavaScript/Book_TheDefinitiveGuide/Basics.js
+++ JavaScript/Book_TheDefinitiveGuide/Basics.js
@@ -127,4 +127,23 @@ points.dist = function() { // Define a method to compute distance between points
 };
 points.dist()   // => Math.sqrt(2): distance between our 2 points
 
+// OOP with JavaScript
+class Point {  // By convection, class name are capitalized.
+    constructor(x,y){  // Constructor function to initialize new instances.
+        this.x = x;   // This keyword is the new object being initialized.
+        this.y = y;  // Store function arguments as object properties.
+    }  // No return is necessary in constructor functions
 
+    distance(){ // Method to compute distance from origin to point
+        return Math.sqrt( // Return the square root of x^2 + y^2
+            this.x * this.x + // this refers to the point object on which
+            this.y * this.y // the distance method is invoked
+        );
+    }
+}
+
+// Use the Point() constructor function with "new" to create Point objects
+let p = new Point(1, 1); // The geometric point (1,1)
+
+// Now use a method of the Point object p
+p.distance();   // => Math.SQRT2
