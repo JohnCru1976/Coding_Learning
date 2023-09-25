4db3d33f69144514d9b92f35f20e1c782574a429
JohnCru1976 <juanbcru@gmail.com>
2023-09-25 06:16:56 +0200

    Update

--- JavaScript/Tests/Frames/scriptFrame.js
+++ JavaScript/Tests/Frames/scriptFrame.js
@@ -1,4 +1,33 @@
+let frameNumber = 0;
+let path = window.location.pathname;
+
+frameNumber = parseInt(path.substring(path.lastIndexOf('/') + 1).match(/\d+/)[0]);
+
 function changeWeb (nF){
+
+    if(frameNumber === 3){
+        sessionStorage.clear;
+    } else {
+        sessionStorage.clear;
+        // Guardar un valor en sessionStorage
+        sessionStorage.setItem('frame', frameNumber);
+        sessionStorage.setItem('data', `Prueba ${frameNumber}`);
+    }
+     
     window.parent.postMessage(`web${nF}.html`, "*");
 }
+
+window.addEventListener('load', (event) =>{
+    let result = document.getElementById('result');
+    let fromFrame = sessionStorage.getItem('frame');
+    let data = sessionStorage.getItem('data');
+    if (data){
+        let text = `<p>Data from frame ${fromFrame}</p>
+                    <p>Data: ${data}</p>
+                   `;
+        if (result) result.innerHTML = text;
+    }
+    
+});
+
 
