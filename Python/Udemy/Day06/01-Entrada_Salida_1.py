fc39d76e096b6d0c5bbaa82c86ed91a4322bde8e
Juan Bautista Cru Murillo <juanbcru@hotmail.com>
2023-09-30 16:53:55 +0200

    Update

--- Python/Udemy/Day06/01-Entrada_Salida_1.py
+++ Python/Udemy/Day06/01-Entrada_Salida_1.py
@@ -1,8 +1,34 @@
-
+# Método read
+print("** Método read **")
 mi_archivo = open('Prueba.txt')
-
 print(mi_archivo) #-> <_io.TextIOWrapper name='../Prueba.txt' mode='r' encoding='cp1252'>
+archivo_completo = mi_archivo.read()
+print(archivo_completo)
+mi_archivo.close()
 
-contenido = mi_archivo.read()
+# Método readline
+print("** Método readline **")
+mi_archivo = open('Prueba.txt')
+linea_1 = mi_archivo.readline()
+print(linea_1)
+linea_2 = mi_archivo.readline()
+print(linea_2.rstrip())  # Se quita el salto de línea
+linea_3 = mi_archivo.readline()
+print(linea_3.upper())
+mi_archivo.close()
 
-print(contenido)
+# Iteración dentro de un archivo
+print("** Iteración dentro de un archivo **")
+mi_archivo = open('Prueba.txt')
+for linea in mi_archivo:
+    print(linea)
+mi_archivo.close()
+
+# Método readlines
+print("** Método readlines **")
+mi_archivo = open('Prueba.txt')
+lineas = mi_archivo.readlines()  # Es una lista
+print(lineas)
+for linea in lineas:
+    print(linea.rstrip())
+mi_archivo.close()
