4a012b5a87bd1e1b98da481c710be8441eeb89fc
JohnCru1976 <juanbcru@gmail.com>
2023-09-30 17:33:38 +0200

    Update

--- Python/Udemy/Day06/02-Escribir_Archivos.py
+++ Python/Udemy/Day06/02-Escribir_Archivos.py
@@ -1,4 +1,26 @@
+# Método write con parametro "w" (Sobrescribe el archivo)
 archivo = open("prueba1.txt", "w")
-archivo.write("Soy el nuevo texto")
+archivo.write("Soy el nuevo texto\n")
+archivo.write("""Hola
+mundo
+aquí estoy""")
 archivo.close()
 
+# Método writelines (escribe todo seguido)
+archivo = open("prueba2.txt", "w")
+archivo.writelines(["Hola Mundo!!\n", "Esto es una prueba"])
+archivo.close()
+
+# Forma de transferir elementos de una lista a un archivo
+archivo = open("prueba3.txt", "w")
+lista = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"]
+for elemento in lista:
+    archivo.write(f"{elemento}\n")
+archivo.close()
+
+# Parametro "a" -- lleva el cursor hasta el final del texto
+archivo = open("prueba3.txt", "a")
+lista = ["Seis", "Siete", "Ocho", "Nueve", "Diez"]
+for elemento in lista:
+    archivo.write(f"{elemento}\n")
+archivo.close()
