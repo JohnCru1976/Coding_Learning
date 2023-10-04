52b8b9ecb205930ee704caabbea321b1391cb35d
JohnCru1976 <juanbcru@gmail.com>
2023-10-04 16:15:17 +0200

    Update

---
+++ Python/Udemy/Day08_Paquetes_Depuracion/03-Package_Example/ejercicio.py
@@ -0,0 +1,11 @@
+from Paquete_Juan import suma_y_resta
+from Paquete_Juan.subPackage import saludo
+from Paquete_Juan.suma_y_resta import *
+from Paquete_Juan.subPackage.saludo import *
+
+suma_y_resta.suma(4,2)
+suma_y_resta.resta(4,2)
+suma(6,2)
+resta(6,2)
+saludo.hola()
+hola()
