a0749102e37c0c8a3b7861e35f8419c0facae2fb
JohnCru1976 <juanbcru@gmail.com>
2023-10-06 09:01:05 +0200

    Prueba commit

--- Python/Udemy/Day09_Otros_Modulos/01-Collections.py
+++ Python/Udemy/Day09_Otros_Modulos/01-Collections.py
@@ -54,4 +54,4 @@ my_deque.popleft()
 print(my_deque)  #-> deque([1, 3, 4, 1, 2])
 
 my_deque.extend(lista)
-print(my_deque)  #-> deque([1, 3, 4, 1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 2, 2, 2, 2, 5, 5, 5, 5, 5, 7, 7, 7, 7])
+print(my_deque)  #-> deque([1, 3, 4, 1, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 2, 2, 2, 5, 5, 5, 5, 5, 7, 7, 7, 7])
