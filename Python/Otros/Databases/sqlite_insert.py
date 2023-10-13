9204836ce077f3bd984e9aa9954adb658a070c4c
JohnCru1976 <juanbcru@gmail.com>
2023-10-13 04:47:39 +0200

    Update

---
+++ Python/Otros/Databases/sqlite_insert.py
@@ -0,0 +1,61 @@
+''' Here I practise with sqlite using the link
+https://www.sqlitetutorial.net/sqlite-python/
+'''
+
+import sqlite_connection as sc
+import sqlite_tables as st
+
+def create_project(conn, project):
+    """
+    Create a new project into the projects table
+    :param conn: sqlite3.Connection
+    :param project: Tuple (name,begin_date,end_date)
+    :return: project id
+    """
+    sql = ''' INSERT INTO projects(name,begin_date,end_date)
+              VALUES(?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, project)
+    conn.commit()
+    return cur.lastrowid
+
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn: sqlite3.Connection object
+    :param task: Tuple (name,priority,status_id,project_id,begin_date,end_date)
+    :return:
+    """
+
+    sql = ''' INSERT INTO tasks(name,priority,status_id,project_id,begin_date,end_date)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    conn.commit()
+
+    return cur.lastrowid
+
+if __name__ == "__main__":
+    # Connection object
+    sc.db_connection = sc.create_connection_disk(sc.db_path)
+
+    # Create tables
+    st.create_tables()
+   
+    # create a new project
+    project = ('Cool App with SQLite & Python', '2015-01-01', '2015-01-30');
+    project_id = create_project(sc.db_connection, project)
+
+    # tasks
+    task_1 = ('Analyze the requirements of the app', 1, 1, project_id, '2015-01-01', '2015-01-02')
+    task_2 = ('Confirm with user about the top requirements', 1, 1, project_id, '2015-01-03', '2015-01-05')
+
+    # create tasks
+    task_1_id = create_task(sc.db_connection, task_1)
+    task_2_id = create_task(sc.db_connection, task_2)
+
+    # Test
+    print(f"The project id is {project_id}")
+    print(f"The task 1 id is {task_1_id}")
+    print(f"The task 2 id is {task_2_id}")
+    
--- Python/Otros/Databases/sqlite_tables.py
+++ Python/Otros/Databases/sqlite_tables.py
@@ -4,7 +4,61 @@ https://www.sqlitetutorial.net/sqlite-python/
 
 import sqlite_connection as sc
 
+# Se crean variables con el código SQL para la creación de las tablas
+sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    begin_date text,
+                                    end_date text
+                                ); """
 
+sql_create_tasks_table = """CREATE TABLE IF NOT EXISTS tasks (
+                                id integer PRIMARY KEY,
+                                name text NOT NULL,
+                                priority integer,
+                                status_id integer NOT NULL,
+                                project_id integer NOT NULL,
+                                begin_date text NOT NULL,
+                                end_date text NOT NULL,
+                                FOREIGN KEY (project_id) REFERENCES projects (id)
+                            );"""
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        # El objeto cursor es una interfaz entre la BD y la aplicación
+        # Su objeto es permitir realizar consultas y recuperar datos 
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except sc.Error as e:
+        print(e)
+
+def create_tables():
+     # create tables
+    if sc.db_connection:
+        # create projects table
+        create_table(sc.db_connection, sql_create_projects_table)
+
+        # create tasks table
+        create_table(sc.db_connection, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
 
 if __name__ == "__main__":
+    # Connection object
     sc.db_connection = sc.create_connection_disk(sc.db_path)
+
+    # create tables
+    if sc.db_connection:
+        # create projects table
+        create_table(sc.db_connection, sql_create_projects_table)
+
+        # create tasks table
+        create_table(sc.db_connection, sql_create_tasks_table)
+    else:
+        print("Error! cannot create the database connection.")
+
