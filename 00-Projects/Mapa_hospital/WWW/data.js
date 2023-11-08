f668e534e9fd08f79c033f9b445c731776a1aa4d
Juan Bautista Cru Murillo <juanbcru@gmail.com>
2023-11-08 13:11:25 +0100

    Update

--- 00-Projects/Mapa_hospital/WWW/data.js
+++ 00-Projects/Mapa_hospital/WWW/data.js
@@ -74,7 +74,7 @@ var last_updates = ["30-10-2023 Añadido el punto Analíticas (Gracias por la co
 "06-11-2023 Se modifica Ecografías: se indica que el sobre se puede entregar en consulta 32",
 "06-11-2023 Se modifica Nutrición y Dietética: se indica que está en el mismo lugar que trauma",
 "07-11-2023 Se quita tutorial de Youtube y se añade enlace a APK para Android v.1.00",
-"07-11-2023 La actual APK es una actualización v.1.03: ahora se borra el caché de forma automática y se confirma el envío el datos con un mensaje",
+"07-11-2023 Actualización de la APK a la v.1.03: ahora se borra el caché de forma automática y se confirma el envío el datos con un mensaje",
 "07-11-2023 Mapa modificado para ubicar correctamente el edificio de maternidad y puntos asociados",
 "07-11-2023 Actualización de la APK a la v.1.04: se ha añadido un texto en la parte superior izquierda con la versión de la APK para saber cuando actualizarla",
 "08-11-2023 Actualización de la APK a la v.1.05: se ha añadido un mensaje de error cuando no hay conexión y no se carga la página. La app de Android funciona cargando la web online y necesita conexión",
