e251dfe106a6e334ad16d2ab49ea80f244f8d6fb
JohnCru1976 <juanbcru@gmail.com>
2024-09-23 20:43:12 +0200

    Adding Variables in Kotlin

---
+++ Kotlin/00_Initial_Tour/Variables.kt
@@ -0,0 +1,13 @@
+// Val - Read Only
+// Var - Mutable
+
+val popcorn = 5    // There are 5 boxes of popcorn
+val hotdog = 7     // There are 7 hotdogs
+var customers = 10 // There are 10 customers in the queue
+
+fun main(){
+    // Some customers leave the queue
+    customers = 8
+    println(customers)
+    // 8
+}
