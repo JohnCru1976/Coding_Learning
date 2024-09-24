e3fb3410195cf61170ab4695e778604da058073f
JohnCru1976 <juanbcru@gmail.com>
2024-09-24 06:51:55 +0200

    Upadte Maps

--- Kotlin/00_Initial_Tour/004_Collections/03_Maps.kt
+++ Kotlin/00_Initial_Tour/004_Collections/03_Maps.kt
@@ -21,10 +21,35 @@ fun main(){
     // of a mutable map by assigning it to a Map
     val juiceMenuLocked: Map<String, Int> = juiceMenu
 
-    // *** ACCESS ***
+    // *** ACCESS Indexed Access Operator []***
     // To access a value in a map, use the indexed access operator [] with its key:
     // Read-only map
     println("The value of apple juice is: ${readOnlyJuiceMenu["apple"]}")
     // The value of apple juice is: 100
+
+    // If you try to access a key-value pair with a key 
+    // that doesn't exist in a map, you see a null value:
+    // Read-only map
+    println("The value of pineapple juice is: ${readOnlyJuiceMenu["pineapple"]}")
+    // The value of pineapple juice is: null
+
+    // *** ADD ***
+    juiceMenu["coconut"] = 150 // Add key "coconut" with value 150 to the map
+    println(juiceMenu)
+    // {apple=100, kiwi=190, orange=100, coconut=150}
+
+    // *** REMOVE ***
+    juiceMenu.remove("orange")    // Remove key "orange" from the map
+    println(juiceMenu)
+    // {apple=100, kiwi=190}
+
+    // *** COUNT ***
+    // Read-only map
+    println("This map has ${readOnlyJuiceMenu.count()} key-value pairs")
+    // This map has 3 key-value pairs
+
+    // *** CHECK KEY ***
+    println(readOnlyJuiceMenu.containsKey("kiwi"))
+    // true
 }
 
