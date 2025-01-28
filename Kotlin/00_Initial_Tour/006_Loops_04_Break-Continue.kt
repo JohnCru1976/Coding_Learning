3de6c32b6cf895457003229637844f284c0a6996
JohnCru1976 <juanbcru@gmail.com>
2025-01-28 01:50:50 +0100

    Update

--- Kotlin/00_Initial_Tour/006_Loops_04_Break-Continue.kt
+++ Kotlin/00_Initial_Tour/006_Loops_04_Break-Continue.kt
@@ -1,3 +1,54 @@
 /*
-
+    28-01-2025
+    Break - Continue - @ Annotation
  */
+
+ fun main(){
+    breakExample()
+ }
+
+// BREAK
+ fun breakExample(){
+    println("BREAK EXAMPLE")
+    var j=10
+
+    for(i in 0..100){
+        j+=j
+
+        if(j>100){
+            break
+        }
+    }
+
+    println("j=$j")
+ }
+
+ // CONTINUE
+ fun continueExample(){
+    println("CONTINUE EXAMPLE")
+    var i = 1
+
+    while(i<20){
+        i+=1
+
+        if(i % 2 != 0){ // Odd number
+            continue // Continue if odd
+        }
+
+        println("i = $i") // Only even numbers
+    }
+ }
+
+ // BREAK AND CONTINUE LABELS
+ fun labelsExample(){
+    println("LABES EXAMPLE")
+
+    outerloop@ for(i in 1..100){
+        println("Outer loop i=$i")
+
+        for(j in 1..100){
+            println("Inner loop j=$j")
+            if(j==10) break@outerloop            
+        }
+    }
+ }
